4a3dcdc62da8c6801b4dec8e41affa2a1f0b99d1
delayedaa <delayedaa@gmail.com>
2020-11-02 12:19:51 -0600

    [app.js] Create function to handle assignment of mystery person

--- js/app.js
+++ js/app.js
@@ -5,5 +5,22 @@ let gameOver = false;
 let player1 = true;
 let player1Score = 0;
 let player2Score = 0;
+let player1MysteryPerson;
+let player2MysteryPerson;
+
+// Functions
+
+let assignMysterPerson = () => {
+  let randomNum1 = Math.floor(Math.random() * allPeople.length);
+  let randomNum2 = Math.floor(Math.random() * allPeople.length);
+  while (randomNum2 === randomNum1) {
+    randomNum2 = Math.floor(Math.random() * allPeople.length);
+  }
+  player1MysteryPerson = allPeople[randomNum1];
+  player2MysteryPerson = allPeople[randomNum2];
+}
+
+assignMysterPerson();
+console.log(player1MysteryPerson, player2MysteryPerson);
 
 });
