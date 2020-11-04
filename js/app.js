51ba77b71909edd9a62cd7b1caf3a108787f6734
delayedaa <delayedaa@gmail.com>
2020-11-04 10:23:06 -0600

    [index.html] Build simplified layout with HTML and connect JavaScript to it

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+
+
+  // Variables and DOM elements
 
   let twoPlayers = false;
   let gameOver = false;
@@ -7,85 +10,158 @@ document.addEventListener('DOMContentLoaded', () => {
   let player2Score = 0;
   let player1MysteryPerson;
   let player2MysteryPerson;
-  let potentialMysteryPerson = allPeople;
-  let computerFirstQuestion = false;
+  let possibilities = allPeople;
+  let computerFirstQuestion = true;
+  let feature;
+  let adjective;
 
-  let searchableKeys = Object.keys(allPeople[0]).filter(element => {
-    if (element !== 'name' && element !== 'gender') {
+  let inputFeature = document.getElementById('user-feature');
+  let inputAdjective = document.getElementById('user-adjective');
+  let inputUserResponse = document.getElementById('user-response');
+  let buttonAsk = document.getElementById('button-ask');
+  let buttonNext = document.getElementById('button-next');
+  let buttonAnswer = document.getElementById('button-answer');
+  let divUserQuestion = document.getElementById('div-user-question');
+  let divNextQuestion = document.getElementById('div-next-question');
+  let divUserResponse = document.getElementById('div-user-response');
+  let currentQuestion = document.getElementById('current-question');
+  let currentResponse = document.getElementById('current-response');
+
+  let searchableKeys = Object.keys(allPeople[0]).filter(key => {
+    if (key !== 'name' && key !== 'gender') {
       return true;
     } else {
       return false;
     }
   });
 
-  // Functions
+
+
+  // Helper functions
+
+  let pickRandomNum = maxNum => {
+    return Math.floor(Math.random() * maxNum);
+  };
+
+  let displayQuestion = () => {
+    if (feature === 'gender') {
+      currentQuestion.textContent = `Is your person a ${adjective}?`;
+    } else if (feature.endsWith('s') || feature === 'hair') {
+      if (!adjective) {
+        currentQuestion.textContent = `Does your person have ${feature}?`;
+      } else {
+        currentQuestion.textContent = `Does your person have ${adjective} ${feature}?`;
+      }
+    } else {
+      if (!adjective) {
+        currentQuestion.textContent = `Does your person have a ${feature}?`;
+      } else {
+        currentQuestion.textContent = `Does your person have a ${adjective} ${feature}?`;
+      }
+    }
+  };
+  
+  let checkForValidQuestion = () => {
+    let test1 = possibilities.some(person => person[feature].length);
+    let test2 = possibilities.every(person => person[feature].includes(adjective));
+    if (!test1) {
+      return false;
+    } else if (test2) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  
+
+
+  // Primary functions
 
   let assignMysteryPerson = () => {
-    let randomNum1 = Math.floor(Math.random() * allPeople.length);
-    let randomNum2 = Math.floor(Math.random() * allPeople.length);
+    let randomNum1 = pickRandomNum(allPeople.length);
+    let randomNum2 = pickRandomNum(allPeople.length);
     while (randomNum2 === randomNum1) {
-      randomNum2 = Math.floor(Math.random() * allPeople.length);
+      randomNum2 = pickRandomNum(allPeople.length);
     }
     player1MysteryPerson = allPeople[randomNum1];
     player2MysteryPerson = allPeople[randomNum2];
   };
 
   let handleUserQuestion = () => {
-    let feature = 'hat';
-    let adjective = '';
-    if (feature === 'gender') {
-      console.log(`Is your person a ${adjective}?`);
-    } else if (feature.endsWith('s') || feature === 'hair') {
-      if (!adjective) {
-        console.log(`Does your person have ${feature}?`);
-      } else {
-        console.log(`Does your person have ${adjective} ${feature}?`);
-      }
-    } else {
-      if (!adjective) {
-        console.log(`Does your person have a ${feature}?`);
-      } else {
-        console.log(`Does your person have a ${adjective} ${feature}?`);
-      }
-    }    
+    feature = inputFeature.value;
+    adjective = inputAdjective.value;
+    displayQuestion();
     if (player2MysteryPerson[feature].includes(adjective) || (!adjective && player2MysteryPerson[feature].length > 0)) {
-      console.log('Yes.');
+      currentResponse.textContent = 'Yes.';
     } else {
-      console.log('No.');
+      currentResponse.textContent = 'No.';
     }
+    // Handle display ————————————————————
+    divUserQuestion.classList.add('hidden');
+    divNextQuestion.classList.remove('hidden');
   };
 
   let handleComputerQuestion = () => {
     if (computerFirstQuestion) {
-      console.log('Is your person a man?');
+      feature = 'gender';
+      adjective = 'man';
+      computerFirstQuestion = false;
     } else {
-      let feature = searchableKeys[Math.floor(Math.random() * searchableKeys.length)];
+      // Pick a feature ————————————————————
+      feature = searchableKeys[pickRandomNum(searchableKeys.length)];
+      // Pick an adjective ————————————————————
       let searchableValues = [];
-      allPeople.forEach(object => {
-        searchableValues = searchableValues.concat(object[feature]);
+      possibilities.forEach(person => {
+        searchableValues = searchableValues.concat(person[feature]);
       });
-      let adjective = searchableValues[Math.floor(Math.random() * searchableValues.length)];
-      if (feature.endsWith('s') || feature === 'hair') {
-        if (!adjective) {
-          console.log(`Does your person have ${feature}?`);
-        } else {
-          console.log(`Does your person have ${adjective} ${feature}?`);
-        }
-      } else {
-        if (!adjective) {
-          console.log(`Does your person have a ${feature}?`);
-        } else {
-          console.log(`Does your person have a ${adjective} ${feature}?`);
-        }
-      }    
+      adjective = searchableValues[pickRandomNum(searchableValues.length)];
+      // Check if the feature and adjective are valid ————————————————————
+      let validQuestion = checkForValidQuestion();
+      while (!validQuestion) {
+        feature = searchableKeys[pickRandomNum(searchableKeys.length)];
+        let searchableValues = [];
+        possibilities.forEach(person => {
+          searchableValues = searchableValues.concat(person[feature]);
+        });
+        adjective = searchableValues[pickRandomNum(searchableValues.length)];
+        validQuestion = checkForValidQuestion();
+      }
     }
+    // Handle display ————————————————————
+    displayQuestion();
+    divNextQuestion.classList.add('hidden');
+    divUserResponse.classList.remove('hidden');
+    currentResponse.textContent = '';
   };
+
+  let handleUserResponse = () => {
+    if (inputUserResponse.value === 'yes') {
+      possibilities = possibilities.filter(person => person[feature].includes(adjective));
+    }
+    // Handle display ————————————————————
+    divUserResponse.classList.add('hidden');
+    divUserQuestion.classList.remove('hidden');
+    currentQuestion.textContent = '';
+    inputFeature.value = '';
+    inputAdjective.value = '';
+    console.log(possibilities); // Test
+  };
+
+
+
+  // Call functions
 
   assignMysteryPerson();
   console.log(player1MysteryPerson, player2MysteryPerson);
 
-  handleUserQuestion();
 
-  handleComputerQuestion();
 
-});
+  // Event listeners
+
+  buttonAsk.addEventListener('click', handleUserQuestion);
+  buttonNext.addEventListener('click', handleComputerQuestion);
+  buttonAnswer.addEventListener('click', handleUserResponse);
+
+
+
+// });
